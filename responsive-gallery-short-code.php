6c35bfb7b05fa6984bc23edb727daa8d16215cb5
weblizar <weblizar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-16 21:11:38 +0000

    gallery layout manged at fronted
    git-svn-id: https://plugins.svn.wordpress.org/responsive-photo-gallery/tags/0.7@949905 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- responsive-gallery-short-code.php
+++ responsive-gallery-short-code.php
@@ -118,60 +118,67 @@ function image_gallery_premium_short_code() {
     <?php while ( $loop->have_posts() ) : $loop->the_post();?>
         <!--get the post id-->
         <?php $post_id = get_the_ID(); ?>
+		<div style="display: block; overflow:hidden;">
+			
+			<!-- gallery title-->
+			<div style="font-weight: bolder; padding-bottom:20px; border-bottom:2px solid #cccccc; margin-bottom: 20px;">
+				<?php echo ucwords(get_the_title($post_id)); ?>
+			</div>
+			
+			<!-- gallery photos-->
+			<div style="">
+				<?php
 
-            <div style="font-weight: bolder;padding-bottom:20px;border-bottom:2px solid #cccccc;margin-bottom: 20px "><?php echo ucwords(get_the_title($post_id)); ?></div>
-    <div class="">
-        <?php
+				/**
+				 * Get All Photos from Gallery Post Meta
+				 */
+				$rpg_all_photos_details = unserialize(get_post_meta( get_the_ID(), 'rpg_all_photos_details', true));
+				//print_r(($rpg_all_photos_details)); echo "<br><br>";
+				$TotalImages =  get_post_meta( get_the_ID(), 'rpg_total_images_count', true );
+				$i = 1;
 
-            /**
-             * Get All Photos from Gallery Post Meta
-             */
-            $rpg_all_photos_details = unserialize(get_post_meta( get_the_ID(), 'rpg_all_photos_details', true));
-            //print_r(($rpg_all_photos_details)); echo "<br><br>";
-            $TotalImages =  get_post_meta( get_the_ID(), 'rpg_total_images_count', true );
-            $i = 1;
+				foreach($rpg_all_photos_details as $rpg_single_photos_detail) {
+					$name = $rpg_single_photos_detail['rpg_image_label'];
+					$url = $rpg_single_photos_detail['rpg_image_url'];
+					?>
+					<div class="<?php echo $WL_Gallery_Layout; ?> col-sm-6 wl-gallery" >
+						<div class="b-link-<?php echo $WL_Hover_Animation; ?> b-animate-go">
 
-            foreach($rpg_all_photos_details as $rpg_single_photos_detail) {
-                $name = $rpg_single_photos_detail['rpg_image_label'];
-                $url = $rpg_single_photos_detail['rpg_image_url'];
+							<img src="<?php echo $url; ?>" class="gall-img-responsive">
+
+							<div class="b-wrapper">
+								<h2 class="b-from-left b-animate b-delay03"><?php echo ucwords($name); ?></h2>
+								<p class="b-from-right b-animate b-delay03">
+									<a href="#" data-toggle="modal" data-target="#<?php echo $post_id."-".$i; ?>">
+										<i class="fa <?php echo $WL_Image_View_Icon; ?> fa-2x"></i>
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<!-- Modal -->
+					<div class="modal fade" id="<?php echo $post_id."-".$i; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h4 class="modal-title" id="myModalLabel"><?php echo ucwords($name); ?></h4>
+								</div>
+								<div class="modal-body">
+									<img src="<?php echo $url; ?>" class="gall-img-responsive" alt="<?php echo ucwords($name); ?>">
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default" data-dismiss="modal"><?php _e("Close", "weblizar_rpg"); ?></button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php
+					$i++;
+				}
 				?>
-                <div class="<?php echo $WL_Gallery_Layout; ?> col-sm-6 wl-gallery" >
-                    <div class="b-link-<?php echo $WL_Hover_Animation; ?> b-animate-go">
-
-                        <img src="<?php echo $url; ?>" class="gall-img-responsive">
-
-                        <div class="b-wrapper">
-                            <h2 class="b-from-left b-animate b-delay03"><?php echo ucwords($name); ?></h2>
-                            <p class="b-from-right b-animate b-delay03">
-                                <a href="#" data-toggle="modal" data-target="#<?php echo $post_id."-".$i; ?>">
-                                    <i class="fa <?php echo $WL_Image_View_Icon; ?> fa-2x"></i>
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Modal -->
-                <div class="modal fade" id="<?php echo $post_id."-".$i; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h4 class="modal-title" id="myModalLabel"><?php echo ucwords($name); ?></h4>
-                            </div>
-                            <div class="modal-body">
-                                <img src="<?php echo $url; ?>" class="gall-img-responsive" alt="<?php echo ucwords($name); ?>">
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal"><?php _e("Close", "weblizar_rpg"); ?></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <?php
-				$i++;
-            }
-        ?>
-        </div>
+			</div>
+		</div>
     <?php endwhile; ?>
     </div>
     <hr>
