6bf255b5fee4254e6c27042e952e0b2730e201d6
weblizar <weblizar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-15 01:59:40 +0000

    commit 2.0 version
    git-svn-id: https://plugins.svn.wordpress.org/responsive-photo-gallery/trunk@1068255 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- responsive-gallery.php
+++ responsive-gallery.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Responsive Photo Gallery
- * Version: 1.9
+ * Version: 2.0
  * Description: Create and display various animated image gallery on WordPress blog.
  * Author: Weblizar
  * Author URI: http://www.weblizar.com
